992f59fbc8aca1a78423861c1d438ed83a7ca184
Adam Veldhousen <adamveld12@gmail.com>
2017-02-26 17:16:09 -0600

    fixed package test

--- pkg.go
+++ pkg.go
@@ -5,39 +5,39 @@ import (
 	"os"
 )
 
-var defaultLogger = New(os.Stdout, false, LFstdFlags)
+var dLogger = New(os.Stderr, false, LFstdFlags)
 
 // Info logs at info level
 func Info(v ...interface{}) {
-	defaultLogger.Info(v...)
+	dLogger.Info(v...)
 }
 
 // Infof logs a formatted string at info level
 func Infof(fmt string, v ...interface{}) {
-	defaultLogger.Infof(fmt, v...)
+	dLogger.Infof(fmt, v...)
 }
 
 // Debug logs at debug level
 func Debug(v ...interface{}) {
-	defaultLogger.Debug(v...)
+	dLogger.Debug(v...)
 }
 
 // Debugf logs a formatted string at debug level
 func Debugf(fmt string, v ...interface{}) {
-	defaultLogger.Debugf(fmt, v...)
+	dLogger.Debugf(fmt, v...)
 }
 
 // SetOutput sets the output writer for the logger
 func SetOutput(w io.Writer) {
-	defaultLogger.SetOutput(w)
+	dLogger.SetOutput(w)
 }
 
 // SetFlags sets the log entry prefix flags
 func SetFlags(f LogFlags) {
-	defaultLogger.SetFlags(f)
+	dLogger.SetFlags(f)
 }
 
 // SetDebug sets debug mode
 func SetDebug(debug bool) {
-	defaultLogger.SetDebug(debug)
+	dLogger.SetDebug(debug)
 }
